ec0e59ad21c8970536db8180a7daea8e71dc5c5e
RRAJ644 <riturajsingh.17248@gmail.com>
2024-07-19 18:00:51 +0530

    cors fix

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ import { routes } from './src/router/index.js'
 
 const app = express()
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
+  // origin: process.env.CORS_ORIGIN,
+  origin: 'http://localhost:5173',
   credentials: true,
 }
 
